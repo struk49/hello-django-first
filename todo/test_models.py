f467f89fd4bdffd60b1b8068396d481db5fc58b0
struk49 <andrewstruk@yahoo.co.uk>
2023-09-24 06:58:30 +0000

    tested if done defaults to false in test_models

---
+++ todo/test_models.py
@@ -0,0 +1,9 @@
+from django.test import TestCase
+from .models import Item
+
+
+class TestModels(TestCase):
+
+    def test_done_defaualts_to_false(self):
+        item = Item.objects.create(name='Test Todo Item')
+        self.assertFalse(item.done)
