6c18f19ba1606133f78e195b6b6eff8a4ce46fcc
struk49 <andrewstruk@yahoo.co.uk>
2023-09-26 10:13:56 +0000

    connected db to app

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -81,7 +81,7 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.environ.get("DATABASE_URL"),
     }
 }
 
